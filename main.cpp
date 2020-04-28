b7d696fdc1e7b79259169469e90ca6f35eb44388
Konstantinos Kalogeropoulos <kkalo878@gmail.com>
2020-04-28 20:34:30 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,19 +1,19 @@
 #include <iostream>
 using namespace std;
 
-//bool  POSSIBLE_COVID_19_INFECTION (User_Trajectory, Day, Catalog){
- //if (){
-
- //}
+bool  POSSIBLE_COVID_19_INFECTION (User_Trajectory, Day, Catalog){
+ if (){
 
 
+ }
 
 
 
-//}
+
+}
 
 int main(){
-    ArrayList^ Catalog = gcnew ArrayList; //Initialize a new ArrayList
+    ArrayList^ Catalog = new ArrayList; //Initialize a new ArrayList
     int width, lngth, area, peri;
     Catalog->Add(1);
     cout<<Catalog;

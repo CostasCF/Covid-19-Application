adfd44dec5ff37db6cc7bed7e86ce081b5aca9d1
Konstantinos Kalogeropoulos <kkalo878@gmail.com>
2020-05-09 15:43:42 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,16 +2,20 @@
 using namespace std;
 
 bool  POSSIBLE_COVID_19_INFECTION (User_Trajectory, Day, Catalog){
- if (){
-
-
+	//Under Construction
+ }
+void FIND_CROWDED_PLACES(Day, Time Interval, Square Region of Interest, Minimum Stay Duration){
+	//Under Construction 
  }
 
-
-
+void  REPAIR (Day, User Trajectory){}
+	//Under Construction
 
 }
 
+void  SUMMARIZE_TRAJECTORY(DAY, DAYS BEFORE, USER TRAJECTORY){
+	//Under Construction
+}
 int main(){
     ArrayList^ Catalog = new ArrayList; //Initialize a new ArrayList
     int width, lngth, area, peri;

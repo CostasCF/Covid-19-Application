fa92b5e754d26f5d2f90eeaef5789431fea8182c
Konstantinos Kalogeropoulos <kkalo878@gmail.com>
2020-05-09 17:31:01 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,111 @@
 #include <iostream>
 using namespace std;
+class Waypoint
+{
+    public:
+        float x;
+        float y;
+        float speed; //Ταχύτητα(σε χιλιόμτερα αν ώρα) του χρήστη μέχρι το επόμενο waypoint (αν υπάρχει)
+        int waitTime; //Χρόνος παραμονής(σε λεπτά) στο waypoint
+        int areaSize; //Μέγεθος(σε χιλιόμετρα) της πλευράς της τετράγωνης περιοχής που κινείται ο χρήστης
+        int time[3]; //
+
+        Waypoint(int areaSize, int time[3])
+        {
+            srand((unsigned) time(0));
+            this->areaSize = areaSize;
+            x = (rand() % areaSize-1) + 0;
+            y = (rand() % areaSize-1) + 0;
+            speed = (rand() % 6) + 3;
+            waitTime = (rand() % 180) + 1;
+            this->time = time;
+        }
+		
+			/Αρχικοποίηση λίστας/
+void List_Initialize(List_Pointer* head) {
+    head = NULL;
+};
+
+/Έλεγχος αν η λίστα είναι κενή/
+int List_Empty(List_Pointer head) const {
+    return head == NULL;
+};
+
+/Η λίστα επιστρέφει το περιεχόμενο του κόμβου όπου δείχνει ο δείκτης p/
+int List_Data(List_Pointer p) const {
+    return p->data;
+};
+
+/Εισαγωγή κόμβου στην αρχή/
+int List_Insert_Start(List_Pointer head, User_Trajectory(int in_Time_Record, int in_x, int in_y)) {
+    List_Pointer newnode;
+    newnode = (List_Node*)malloc(sizeof(List_Node));
+    if (!newnode) {
+        cout << ("Αδυναμία δέσμευσης μνήμης");
+        return false;
+    }
+    newnode->data = x;
+    newnode->next = *head;
+    head = newnode;
+    return true;
+};
+
+/Εισαγωγή κόμβου μετά από κάποιον άλλον/
+int List_Insert_After(List_Pointer p, int k) {
+    List_Pointer newnode;
+    newnode = (List_Node)malloc(sizeof(List_Node));
+    if (!newnode) {
+        cout << ("Αδυναμία δέσμευσης μνήμης");
+        return false;
+    };
+    newnode->data = x;
+    newnode->next = p->next;
+    p->next = newnode;
+    return true;
+}
+
+/Διαγραφή του πρώτου κόμβου/
+int List_Delete_Start(List_Pointer* head, int k) {
+    List_Pointer current;
+    if (*head == NULL) {
+        return false;
+    };
+    current = *head;
+    k = current->data;
+    (head) = (head)->next;
+    free(current);
+    return true;
+};
+
+/Διαγραφή ενδιάμεσου κόμβου*/
+int List_Delete_After(List_Pointer prev, int k) {
+    List_Pointer current;
+    if (prev->next == NULL) {
+        return false;
+    };
+    current = prev->next;
+    k = current->data;
+    prev->next = current->next;
+    free(current);
+    return false;
+};
+
+/Καταστροφή λίστας/
+void List_Destroy(List_Pointer head) {
+    List_Pointer ptr;
+    while (*head != NULL) {
+        ptr = *head;
+        head = (head)->next;
+        free(ptr);
+    }
+}
+struct node {
+            int data;
+            struct node* next;
+        };
+typedef struct node List_Node;
+        typedef struct node* List_Pointer;
+};
 
 bool  POSSIBLE_COVID_19_INFECTION (User_Trajectory, Day, Catalog){
 	//Under Construction
@@ -17,9 +123,5 @@ void  SUMMARIZE_TRAJECTORY(DAY, DAYS BEFORE, USER TRAJECTORY){
 	//Under Construction
 }
 int main(){
-    ArrayList^ Catalog = new ArrayList; //Initialize a new ArrayList
-    int width, lngth, area, peri;
-    Catalog->Add(1);
-    cout<<Catalog;
 
 }

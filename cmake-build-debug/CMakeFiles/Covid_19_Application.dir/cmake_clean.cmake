3a6f8ecc654e85cadc430c3e7e8cc5df9695baf3
Konstantinos Kalogeropoulos <kkalo878@gmail.com>
2020-05-11 05:26:54 -0700

    Added clion files

---
+++ cmake-build-debug/CMakeFiles/Covid_19_Application.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Covid_19_Application.dir/LinkedList.cpp.obj"
+  "CMakeFiles/Covid_19_Application.dir/main.cpp.obj"
+  "Covid_19_Application.exe"
+  "Covid_19_Application.exe.manifest"
+  "Covid_19_Application.pdb"
+  "libCovid_19_Application.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Covid_19_Application.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
